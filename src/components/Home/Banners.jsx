cf18d005c4d43f8caf371e2e08830eaec6de8467
AZMSHA <azmsha2003@gmail.com>
2024-06-05 10:54:17 +0500

    banners added

---
+++ src/components/Home/Banners.jsx
@@ -0,0 +1,24 @@
+import "./Banners.css";
+
+function Banners() {
+  return (
+    <section className="banners">
+      <article className="banner1">
+        <h2>
+          choose the best <span>cycling gloves</span>
+        </h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+        <button type="button">view all products</button>
+      </article>
+      <article className="banner2">
+        <h2>
+          choose the best <span>cycling shoes</span>
+        </h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+        <button type="button">view all products</button>
+      </article>
+    </section>
+  );
+}
+
+export default Banners;
